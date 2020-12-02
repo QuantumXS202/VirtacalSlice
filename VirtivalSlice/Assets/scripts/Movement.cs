d95de2e4d7fa551e96baab884d98fbb10b5a0d17
QuantumXS202 <Quinncroese@gmail.com>
2020-12-02 09:55:42 +0100

    iets

---
+++ VirtivalSlice/Assets/scripts/Movement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float moveSpeed = 2f;
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    public GameObject bulletPrefab;
+    public float bulletSpeed;
+
+    private float lastFire;
+
+    public float fireDelay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        float shootHor = Input.GetAxis("ShootHorizontal");
+        float shootVert = Input.GetAxis("ShootVertical");
+
+        if((shootHor !=0 || shootVert !=0) && Time.time > lastFire + fireDelay)
+        {
+            Shoot(shootHor, shootVert);
+            lastFire = Time.time;
+        }
+        
+    }
+
+    void FixedUpdate() 
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void Shoot(float x, float y)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation) as GameObject;
+        bullet.AddComponent<Rigidbody2D>().gravityScale = 0;
+        bullet.GetComponent<Rigidbody2D>().velocity = new Vector3(
+        (x < 0) ? Mathf.Floor(x) * bulletSpeed : Mathf.Ceil(x) * bulletSpeed,
+        (y < 0) ? Mathf.Floor(y) * bulletSpeed : Mathf.Ceil(y) * bulletSpeed, 0);  
+    }
+}
