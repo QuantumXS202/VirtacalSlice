c3c678e0b9b90e6c339c44a8bca0c923a090aa77
Miguel-Heijmans <58071327+Miguel-Heijmans@users.noreply.github.com>
2020-12-14 20:20:19 +0100

    isaac schiet nu tears met werkende anims

---
+++ VirtivalSlice/Assets/scripts/Schooting/Tear.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tear : MonoBehaviour
+{
+
+    private bool hit;
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        hit = true;
+        Invoke("DestroyIt", 0.5f);
+    }
+
+    public void DestroyIt()
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("hit", hit);
+    }
+}
